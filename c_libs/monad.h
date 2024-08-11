802bed22522103c6f3d1a782c1f6111d4f8ccd33
thorped1 <d.thorpe3@lancaster.ac.uk>
2024-08-11 11:45:13 +0100

    Added C++ library for monadic IO actions

---
+++ c_libs/monad.h
@@ -0,0 +1,55 @@
+#ifndef MONAD
+#define MONAD
+
+#include <vector>
+#include <functional>
+#include <stdint.h>
+
+
+template <typename T>
+class IOMonad {
+    private:
+        T value;
+        std::vector<std::function<T(T)>> action;
+    
+        IOMonad(T value);
+
+
+    public:
+        static IOMonad<T> lift(T value);
+        T bind();
+        void arrow(T (*func)(T));
+};
+
+
+template <typename T>
+IOMonad<T>::IOMonad(T value) : value(value) {}
+
+
+template <typename T>
+IOMonad<T> IOMonad<T>::lift(T value) {
+    return IOMonad<T>(value);
+}
+
+
+template <typename T>
+T IOMonad<T>::bind() {
+    if (this->action.size() == 0)
+        return this->value;
+    
+    T intermediate = this->value;
+    for (uint32_t i = 0; i < this->action.size(); i++) {
+        intermediate = this->action[i](intermediate);
+    }
+    
+    return intermediate;
+}
+
+
+template <typename T>
+void IOMonad<T>::arrow(T (*func)(T)) {
+    this->action.push_back(func);
+}
+
+
+#endif
